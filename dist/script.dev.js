a683e8f717b7f0a4d6584317a3e964e0d0195865
nuke7 <gombos.marton@icloud.com>
2020-12-17 11:23:22 +0100

    added today's live coding samples

---
+++ dist/script.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function pageLoad(params) {
+  var root = document.querySelector("#root");
+  var notes = [];
+  notes.push({
+    tag: "div",
+    content: "always link css and js in the head"
+  });
+  notes.push({
+    tag: "pre",
+    content: "<code>\n    &lt;link rel=\"preconnect\" href=\"https://fonts.gstatic.com\"/>\n    &lt;link\n      href=\"https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap\"\n      rel=\"stylesheet\"\n    />\n    &lt;link rel=\"stylesheet\" href=\"./css/style.css\" />\n    &lt;script src=\"./script.js\">&lt;/script>\n    </code>"
+  });
+
+  for (var _i = 0, _notes = notes; _i < _notes.length; _i++) {
+    note = _notes[_i];
+    root.insertAdjacentHTML("beforeend", "\n    <".concat(note.tag, "> ").concat(note.content, " </").concat(note.tag, ">"));
+  }
+}
+
+window.addEventListener("load", pageLoad);
+/* document.querySelectorAll('code').forEach((block) => {
+  hljs.highlightBlock(block);
+});
+ */
+
+/*
+document.querySelectorAll('code').forEach((block) => {
+  hljs.highlightBlock(block);
+});
+*/
+//Az összes Js note-ot ilyen módon kirendelelni és a style.scss-ben megformázni.
+
+/*
+//Optional: Custom in. - HL JS beágyazása.
+
+https://highlightjs.org/usage/
+https://highlightjs.org/download/
+<link rel="stylesheet"
+    href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/highlight.min.js"></script>
+*/
---
+++ script.js
@@ -0,0 +1,54 @@
+function pageLoad(params) {
+  let root = document.querySelector("#root");
+  let notes = [];
+
+  notes.push({
+    tag: "div",
+    content: `always link css and js in the head`,
+  });
+
+  notes.push({
+    tag: "pre",
+    content: `<code>
+    &lt;link rel="preconnect" href="https://fonts.gstatic.com"/>
+    &lt;link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    &lt;link rel="stylesheet" href="./css/style.css" />
+    &lt;script src="./script.js">&lt;/script>
+    </code>`,
+  });
+
+  for (note of notes) {
+    root.insertAdjacentHTML(
+      "beforeend",
+      `
+    <${note.tag}> ${note.content} </${note.tag}>`
+    );
+  }
+}
+
+window.addEventListener("load", pageLoad);
+
+/* document.querySelectorAll('code').forEach((block) => {
+  hljs.highlightBlock(block);
+});
+ */
+
+/*
+document.querySelectorAll('code').forEach((block) => {
+  hljs.highlightBlock(block);
+});
+*/
+
+//Az összes Js note-ot ilyen módon kirendelelni és a style.scss-ben megformázni.
+/*
+//Optional: Custom in. - HL JS beágyazása.
+
+https://highlightjs.org/usage/
+https://highlightjs.org/download/
+<link rel="stylesheet"
+    href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/highlight.min.js"></script>
+*/
